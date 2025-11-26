100529079cacde9d4cea0dfe15f3cbde63156f01
hridyesh-gupta <hridyesh.gupta1206@gmail.com>
2025-11-26 21:35:05 +0530

    Minor changes

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
     unoptimized: true,
   },
   typescript: {},
+  experimental: {
+    // Reduce Next.js static generation concurrency for constrained build environments
+    workerThreads: false,
+    cpus: 1,
+  },
 };
 
 module.exports = nextConfig;
